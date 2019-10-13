9e5809b126b374a9e564031b0a8bb158a3c9a327
youssef idder <ucef.idder96@gmail.com>
2019-10-13 12:27:21 +0100

    Initial commit

---
+++ src/main/java/ensa/mobile/ivisitservice/beta/IVisitApplication.java
@@ -0,0 +1,13 @@
+package ensa.mobile.ivisitservice.beta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IVisitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IVisitApplication.class, args);
+    }
+
+}
